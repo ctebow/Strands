eb842a0aa49b51430b325428fd3018e33acafcfc
James Rosenberger <jamesrosenberger@Jamess-MacBook-Pro.local>
2025-05-05 18:31:38 -0500

    Logic implementation passes all test cases, though have not deleted unnecessary comments or ran on pylint or mypy

--- strands-main/src/fakes.py
+++ strands-main/src/fakes.py
@@ -11,13 +11,13 @@ class Pos(PosBase):
         c = self.c
         r = self.r
 
-        if "w" in step:
+        if "w" in step.value:
             c -= 1
-        if "n" in step:
+        if "n" in step.value:
             r -= 1
-        if "s" in step:
+        if "s" in step.value:
             r += 1
-        if "e" in step:
+        if "e" in step.value:
             c += 1
 
         return Pos(r, c)
@@ -36,34 +36,25 @@ class Pos(PosBase):
 
             if r_dist == -1:
                 step += "s"
-            else:
+            elif r_dist == 1:
                 step += "n"
             if c_dist == -1:
                 step += "e"
-            else:
+            elif c_dist == 1:
                 step += "w"
         
-            return step
+            return Step(step)
 
         else:
             raise ValueError
     
     def is_adjacent_to(self, other: "Pos") -> bool:
 
-        # this is an interesting alternative - what do you think?
-        # try:
-        #     self.step_to(other)
-        #     return True
-
-        # except ValueError:
-        #     return False
-
-        r_dist = self.r - other.r
-        c_dist = self.c - other.c
-
-        if abs(r_dist) == 1 or abs(c_dist) == 1:
+        try:
+            self.step_to(other)
             return True
-        else:
+
+        except ValueError:
             return False
         
 class StrandFake(StrandBase):
@@ -72,7 +63,7 @@ class StrandFake(StrandBase):
 
         stack = []
         stack.append(self.start)
-        position_list = []
+        position_list = [self.start]
 
         for step in self.steps:
             pos = stack.pop().take_step(step)
@@ -131,5 +122,156 @@ class BoardFake(BoardBase):
     def evaluate_strand(self, strand: StrandFake) -> str:
         raise NotImplementedError
 
-class StrandsGameFake(StrandsGameBase):
-    ...
+class StrandsGameFake(StrandsGameBase): # add extension back at the end from StrandsGameBase
+
+    game_theme: str
+    hint_thresh: int
+    game_board: list[list[str]]
+    game_answers: list[tuple[str, StrandFake]]
+    tot_game_guesses: list[tuple[str, StrandFake]] # biggest issue is it currently does not 
+                                                   # include strand guesses, since 
+                                                   # dictionary not implmeneted
+    hint_state: None | bool
+    hint_word: str
+    # guesses made after hint cleared
+    new_game_guesses: list[tuple[str, StrandFake]]
+
+    def __init__(self, game_file: str | list[str], hint_threshold: int = 3):
+
+        if isinstance(game_file, str):
+            with open(game_file, encoding="utf-8") as f:
+                lines_lst: list[str] = [line.strip() for line in f.readlines()]
+        else:
+            lines_lst = [line.strip() for line in game_file]
+
+        # print(lines_lst)
+        self.game_theme = lines_lst[0]
+        self.hint_thresh = hint_threshold
+
+        board_lst: list[list[str]] = []
+
+        # assumes one space before grid in valid file
+        for ind, r in enumerate(lines_lst[2:]):
+            if r == "":
+                board_stp = ind + 3
+                break
+            
+            alph_lst = [alph.lower() for alph in r.split()]
+            board_lst.append(alph_lst)
+
+        self.game_board = board_lst
+
+        game_answers: list[tuple[str, StrandFake]] = []
+        for ind, r in enumerate(lines_lst[board_stp:]):
+            if r == "":
+                break
+
+            steps: list[Step] = []
+            full = r.split()
+            word = full[0].lower()
+
+            # adjusting to 0-indexing
+            start = Pos(int(full[1]) - 1, int(full[2]) - 1)
+            steps.extend([Step(dirc.lower()) for dirc in full[3:]])
+
+            game_answers.append((word, StrandFake(start, steps)))
+
+        self.game_answers = game_answers
+        self.tot_game_guesses = []
+        self.hint_state = None
+        self.hint_word = self.game_answers[0][0]
+        self.new_game_guesses = []
+
+    def theme(self) -> str:
+        return self.game_theme
+
+    def board(self) -> BoardBase:
+        return BoardFake(self.game_board)
+    
+    def answers(self) -> list[tuple[str, StrandBase]]:
+        return self.game_answers
+
+    def found_strands(self) -> list[StrandBase]:
+
+        found_strands = []
+        for gus_word, _ in self.tot_game_guesses:
+            for ans_word, ans_strd in self.game_answers:
+                if gus_word == ans_word:
+                    # appends desired ans_strd, even if different from guess
+                    found_strands.append(ans_strd)
+                    
+        return found_strands
+    
+    def game_over(self) -> bool:
+
+        if len(self.found_strands()) == len(self.answers()):
+            return True
+        
+        return False
+
+    def hint_threshold(self) -> int:
+        return self.hint_thresh
+    
+    def hint_meter(self) -> int:
+        level = len(self.new_game_guesses)
+        if level >= self.hint_threshold():
+            print("You can request a hint!")
+
+        return level
+
+    def active_hint(self) -> None | tuple[int, bool]:
+
+        # theme words found thus far
+        cur_theme_strds = self.found_strands()
+
+        if self.hint_state is None:
+            return None
+
+        for ind, (word, strd) in enumerate(self.answers()):
+            if strd not in cur_theme_strds:
+                i = ind
+                self.hint_word = word
+                return (i, self.hint_state)
+
+    def submit_strand(self, strand: StrandBase) -> tuple[str, bool] | str:
+
+        pos = strand.start
+
+        # ensures pos arguments of strand exist on board
+        try:
+            self.board().get_letter(pos)
+        except ValueError:
+            return "Not a theme word"
+        
+        for word, strd in self.answers():
+            if word[0] == self.game_board[pos.r][pos.c]:
+                if strd not in self.found_strands():
+                    self.tot_game_guesses.append((word, strd))
+                    self.new_game_guesses.append((word, strd))
+                    # theme word is found basic imp
+                    if word[0] == self.hint_word[0]:
+                        # clearing the hint
+                        self.hint_state = None
+                        self.new_game_guesses = []
+
+                    return (word, True)
+                else:
+                    return "Already found"
+        
+        return "Not a theme word"
+
+    def use_hint(self) -> tuple[int, bool] | str:
+
+        if self.active_hint() is None:
+            # next step in active_hint
+            self.hint_state = False
+            return self.active_hint()
+
+        assert self.active_hint() is not None
+        _, cond = self.active_hint()
+        if cond is False:
+            # next step in active_hint
+            self.hint_state = True
+            return self.active_hint()
+        else:
+            return "Use your current hint"
