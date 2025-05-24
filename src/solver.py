515718a16b7af2eadbcae874d0af59cff44f26ef
Caden Tebow <cadentebow@Cadens-MacBook-Air.local>
2025-05-24 18:52:43 -0500

    Added word dictionary and worked on solver

--- src/solver.py
+++ src/solver.py
@@ -45,12 +45,55 @@ class Trie:
                 node.children[char] = TrieNode(char)
             node = node.children[char]
         
-        node.is_end = True       
+        node.is_end = True    
+
+class Mask:
+    """
+    Integer mask intended to represent a typical strands gameboard. More
+    efficient than using a list to keep track of positions
+    """   
+
+    def __init__(self, rows, cols):
+        self.mask = 0
+        self.rows = rows
+        self.cols = cols
+    
+    def get_mask(self) -> int:
+        """
+        Get the mask. 
+        """
+        return self.mask
+
+    def set_val(self, r: int, c: int) -> None:
+        """
+        Set a bit value at index r, c in the bitmask. 
+        """
+        # bitwise shift to index
+        mask = self.mask
+        self.mask = mask | (1 << (r * self.cols + c)) 
+
+    def clear_val(self, r: int, c: int) -> None:
+        """
+        Clear a bit value at index r, c in the bitmask.
+        """
+        mask = self.mask
+        self.mask = mask & ~(1 << (r * self.cols + c))
+    
+    def is_val(self, r: int, c: int) -> bool:
+        """
+        Check if a certain position in the mask is set to true. 
+        """
+        return self.mask >> (r * self.cols + c) & 1
+    
+    def reset(self) -> None:
+        """
+        Reset the mask
+        """
+        self.mask = 0
 
 class Solver:
 
     game_file: str | list[str]
-    #board: Board
     board_lst = list[list[str]]
     dictionary: list[str]
     cols: int
@@ -78,11 +121,19 @@ class Solver:
 
         # get word list --> to be put in Trie. 
         with open("assets/web2.txt", encoding="utf-8") as f:
-            word_dictionary = [line.strip() for line in f.readlines()]
+            word_dictionary = [line.strip().lower() for line in f.readlines()]
+
+        # get frequecy chart of top 50k words
+        frequency_chart = {}
+        with open("assets/en_50k.txt", encoding="utf-8") as f:
+            for line in f.readlines():
+                lst = line.split()
+                if len(lst[0]) > 3:
+                    frequency_chart[lst[0]] = int(lst[1])
 
         self.dictionary = word_dictionary
+        self.frequency_chart = frequency_chart
         self.board_lst = board_lst
-      #  self.board = Board(board_lst)
         self.cols = len(board_lst[0])
         self.rows = len(board_lst)
         self.board_size = self.cols * self.rows
@@ -146,7 +197,39 @@ class Solver:
         
         return all_words
     
-    def can_place(self, strand: Strand, marked) -> bool:
+    def sort_words(self, raw_words: dict[tuple[int, int],
+                                          tuple[Step, ...]]) -> list[Strand]:
+        """
+        Filtering function for all collected words. Uses a frequency score, 
+        "odd" letter combination dictionary, words that are too short or too 
+        long, plural or conjugated forms. Then turns words into strands and 
+        sorts them by largest to smallest. (aim for 6-7 words present.)
+        """
+        # sort words that only are the top 50k in english dictionary. 
+        top_50k = {}
+        for word in raw_words.keys():
+            if word in self.frequency_chart and self.frequency_chart[word] > 200:
+                top_50k[word] = raw_words[word]
+        # pop words that are too long
+        for word in top_50k:
+            if len(word) > 10:
+                top_50k.pop(word)
+
+        # sort and transform word list into strands
+        all_strands = []
+        for word in top_50k.values():
+            start, steps = word
+            r, c = start
+            all_strands.append(Strand(Pos(r, c), list(steps)))
+        
+        # remove invalid strands, sort largest to smallest
+        filtered = list(filter(lambda x: not (x.is_cyclic() or x.is_folded()),
+                                all_strands))
+        filtered.sort(key=lambda x: -len(x.positions()))
+
+        return filtered
+
+    def can_place(self, strand: Strand, mask: Mask) -> bool:
         """
         Helper function for place_words, checks if a strand can be placed on
         the board.
@@ -156,11 +239,11 @@ class Solver:
         for pos in positions:
             r = pos.r
             c = pos.c
-            if marked[r][c]:
+            if mask.is_val(r, c):
                 return False
         return True
     
-    def place(self, strand: Strand, marked) -> int:
+    def place(self, strand: Strand, mask: Mask) -> int:
         """
         Helper function for place_words, places a strand on the marked board.
         """
@@ -168,11 +251,11 @@ class Solver:
         for pos in positions:
             r = pos.r
             c = pos.c
-            marked[r][c] = True
+            mask.set_val(r, c)
 
         return len(positions)
 
-    def remove(self, strand: Strand, marked) -> None:
+    def remove(self, strand: Strand, mask: Mask) -> None:
         """
         Helper function for place_words, removes a strand on the marked board.
         """
@@ -180,54 +263,62 @@ class Solver:
         for pos in positions:
             r = pos.r
             c = pos.c
-            marked[r][c] = False
+            mask.clear_val(r, c)
 
-    
-    def place_words(self, words: dict[tuple[int, int], tuple[any]]) -> set[str]:
+    def place_top_50(self, words: list[Strand]) -> set[str]:
         """
-        Given a list of all the words possible in a board, figure out which 
-        words are the strands. Words must be at least four letters long.
+        Given a list of probable, largest words, place the top 50 and fit from
+        there. 
         """
+        # after we place the top idk 50 combos of words, we can go from those
+        # boards
 
-        # convert all words into strands
-        all_strands = []
-        for word in words.values():
-            start, steps = word
-            r, c = start
-            all_strands.append(Strand(Pos(r, c), list(steps)))
-        
-        # remove invalid strands
-        filtered = list(filter(lambda x: not (x.is_cyclic() or x.is_folded()),
-                                all_strands))
-        # obtain markers
-        marked = [[False] * self.cols for _ in range(self.rows)]
-        solutions = []
+        # represent boards bitwise.
+        mask = Mask(self.rows, self.cols)
+        top_50 = words[:50]
+        boards = []
 
-        # main checking loop:
-        for idx, strand in enumerate(filtered):
-            placed = []
-            coverage = 0
-            if self.can_place(strand, marked):
-                coverage += self.place(strand, marked)
-                placed.append(strand)
-
-            for strand_new in filtered[idx+1:]:
-                if strand == strand_new:
-                    continue
-                if self.can_place(strand_new, marked): # catches case where two are the same
-                    coverage += self.place(strand_new, marked)
-                    placed.append(strand_new)
+        def placer_dfs(strand: Strand, placed: tuple[Strand], mask: Mask):
+            """
+            Depth first algorithm to place top 50 strands on a board. 
+            """
+            if len(placed) > 5:
+                boards.append(placed)
+                return
             
-            if coverage == self.board_size:
-                solutions.append(placed)
-            for strand in placed:
-                self.remove(strand, marked)
+            if self.can_place(strand, mask):
+                self.place(strand, mask)
+                placed_new = placed + (strand,)
+                for strand_new in top_50:
+                    if strand == strand_new:
+                        continue
+                    placer_dfs(strand_new, placed_new, mask)
+                self.remove(strand, mask)
+      
+        for strand in top_50:
+            placer_dfs(strand, (strand,), mask)
 
-        return solutions
+        return boards
+
+
+
+
+
+
+
 
 # Place words not right, need a recursive approach. Will do later
+grrr_ans = ["VEXED",
+"IRKED",
+"SURLY",
+"CRANKY",
+"GRUMPY",
+"PEEVED",
+"TOUCHY",
+"CROSSWORD"]
 
-solved = Solver("boards/grrr.txt")
-words = solved.all_words()
-answer = solved.place_words(words)
-print(answer)
+solver = Solver("boards/grrr.txt")
+raw = solver.all_words()
+sorts = solver.sort_words(raw)
+boards = solver.place_top_50(sorts)
+print(boards)
